93143619cd3d23b7292ccd1861417087079e8d7c
Jaylee Conlin <jconlin1@asu.edu>
2015-01-29 20:43:55 -0700

    Added the tiles for the board

---
+++ Capstone Visual/Capstone Visual/Capstone Visual/Models/Tile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone_Visual.Models
+{
+    public class Tile
+    {
+        public int value;
+        public string imagePath;
+        public string color;
+
+        public Tile()
+        {
+            imagePath = "http://i.imgur.com/iPXQebh.png?1";
+            value = 0;
+        }
+
+    }
+
+    public class TileArray
+    {
+        public Tile[,] TileBoard = new Tile[8,8];
+
+        public TileArray()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    TileBoard[i, j] = new Tile();
+                }
+            }
+        }
+
+
+
+    }
+
+    
+}
